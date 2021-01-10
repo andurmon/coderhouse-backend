2d31fe6bea09fbef0c9f9acd2750de1d99845763
andurmon <andurmon@gmail.com>
2021-01-09 20:38:19 -0500

    Reto clase 0 y 1. Para el reto de la clase 1 se crearon clases con la funcion constructora y con el keyword class de ES6

--- Clase-1 JS y ES6/renderTable.js
+++
@@ -1,6 +0,0 @@
-function renderTable(usuario){
-  document.getElementById("nombre").innerText = usuario.nombre
-  document.getElementById("apellido").innerText = usuario.apellido
-  document.getElementById("mascotas").innerText = usuario.mascotas
-  document.getElementById("libros").innerText = JSON.stringify(usuario.libros)
-}
