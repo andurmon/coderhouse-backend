0790a1d34a6bbdcd167c0e83c2e8fde40b1a9a9e
andurmon <andurmon@gmail.com>
2021-01-17 23:08:48 -0500

    Desafio 3 con num total de palabras

--- Clase3 Asincronismo/desafio/JA+UribeAndres.js
+++ Clase3 Asincronismo/desafio/JA+UribeAndres.js
@@ -1,18 +1,20 @@
 // Se declara el Texto que se usará como una constante
-const texto = "Hola como estan todos ustedes el dia de hoy?";
+const texto1 = "1. Hola como estan todos ustedes el dia de hoy?";
+const texto2 = "2. Muy bien gracias de preguntar";
+const texto3 = "3. Yo bien gracias por no preguntar";
 
 // Callback Hell con tres callbacks anidados
-recorrerTexto(texto, ()=>{
-    recorrerTexto("Pues hombre muy bien la verdad", ()=>{
-        recorrerTexto("Mil gracias que tengas buen día!", ()=> console.log("[SUCCESS] - Proceso Completo"))
-    },250)
-}, 500)
+recorrerTexto(texto1, (params)=>{
+    recorrerTexto(texto2, (params)=>{
+        recorrerTexto(texto3, (params)=> console.log(`[SUCCESS] - Proceso Completo ${params} palabras en total`), params)
+    }, params, 250)
+}, 0, 500)
 
 
 // Funcion recorrer texto que corre de manera asincronica
-function recorrerTexto(texto, cb, time_ms=1000){
+function recorrerTexto(texto, callback, params, time_ms=1000){
     
-    let splittedText = texto.split(' ');
+    let splittedText = texto.split(' '); //Se separa por espacios
     let numWords = splittedText.length;
     let index = 0;
 
@@ -23,12 +25,13 @@ function recorrerTexto(texto, cb, time_ms=1000){
         if(index === numWords){
             clearInterval(intervalId)
             endMessage(numWords, time_ms)
-            cb()
+            callback(params+numWords)
         }
     }, time_ms)
+    return numWords
 }
 
 //Funcion que llamo cada que termina de imrpimir un texto
 function endMessage(numWords, delay_ms){
-    console.log(`[SUCCESS] - Termino la ejecución!, se imprimieron ${numWords} palabras con un retardo de ${delay_ms/1000} segundos entre sí`);
+    console.log(`[SUCCESS] - Se imprimieron ${numWords} palabras con un retardo de ${delay_ms/1000} segundos entre sí`);
 }
